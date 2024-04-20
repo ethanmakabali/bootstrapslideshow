8b14b43c2e764f8f0d8ad4c02d91515e7ef39cfe
ethanmakabali <emakabali@mail.endicott.edu>
2024-04-20 12:14:01 -0400

    boot strap slideshow
    rotating images of frogs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+let intIndex = 0;
+
+function incrementIndex() {
+    intIndex++;
+}
+
+function showImage() {
+    console.log("Showing image with index:", intIndex);
+}
+
 window.onload = function() {
     document.getElementById("copyright").innerHTML = new Date().getFullYear();
+
+    setInterval(function() {
+        showImage();
+        incrementIndex();
+    }, 5000);
 }
